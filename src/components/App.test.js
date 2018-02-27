405c4133d50949cf7aa442ec1b14fe749fac863b
B Terbush <brittanyterbush@gmail.com>
2018-02-26 17:09:52 -0800

    Write first unit test. Add config files to make compatible with React v.16

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,10 +1,12 @@
-// 1. fundamental test for a component - making sure it renders in the DOM
-//
+/**
+ * @jest-environment node
+ */
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
- const app = shallow(<App />);
+const app = shallow(<App />);
 
  // first test - a unit test //
  it('renders correctly', () => {
